19ef17824d1c5d97981e6bc10d075af861732c58
Jamal Salim <Jamalsalim.js12@gmail.com>
2024-02-24 04:05:25 +0000

    Made few change in orders to fit with the new Input Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
-import notes from "./notes";
+import CreateArea from "./components/CreateArea";
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addItems = (noteValue) => {
+    setNotes((prevValue) => [...prevValue, noteValue]);
+  };
+
+  const deleteItems = (id) => {
+    setNotes((prevNotes) => prevNotes.filter((note, index) => index !== id));
+  };
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
+      <CreateArea onAdd={addItems} />
+      {notes.map((note, index) => (
         <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
+          title={note.title}
+          content={note.content}
+          id={index}
+          key={index}
+          onDelete={deleteItems}
         />
       ))}
+
       <Footer />
     </div>
   );
